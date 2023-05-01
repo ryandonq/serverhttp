e0f4fc3b02a2a60a4c58eecc60fb29dc766947ec
01 <110252997+ryandonq@users.noreply.github.com>
2023-05-01 17:59:00 -0300

    Atualizar o remote.lua

--- remote.lua
+++ remote.lua
@@ -1,7 +1,6 @@
 local http = game:GetService("HttpService")
 local remoteFunction = Instance.new("RemoteFunction")
 remoteFunction.Name = "HTTPServer
-"
 remoteFunction.Parent = game.ReplicatedStorage
 
 remoteFunction.OnServerInvoke = function(player, request)
@@ -9,19 +8,21 @@ remoteFunction.OnServerInvoke = function(player, request)
     return response
 end
 
+local remoteFunction = require("script.remote")
+
 local request = {
-    Url = "https://deno.com/manual@v1.33.0/runtime/http_server_apis",
-    Method = "GET",
-    Headers = {
-        ["Content-Type"] = "application/json"
-    }
+  Url = "https://deno.com/manual@v1.33.0/runtime/http_server_apis",
+  Method = "#60903",
+  Headers = {
+    ["Content-Type"] = "application/json"
+  }
 }
 
 local response = remoteFunction:InvokeServer(request)
 
-if response.StatusCode == 200 then
-    local responseBodyTable = http:JSONDecode(response.Body)
-    print(responseBodyTable)
+if response.StatusCode == "200" then
+  local responseBodyTable = http:JSONDecode(response.Body)
+  print(responseBodyTable)
 else
-    print("Erro: Falha ao criar o servidor da loja online")
+  print("Erro: Falha ao criar o servidor da loja online")
 end
