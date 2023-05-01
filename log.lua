90528e68b4e9d1e7a927198677331f42ac26abf4
01 <110252997+ryandonq@users.noreply.github.com>
2023-05-01 17:49:58 -0300

    Atualizar o log.lua

--- log.lua
+++ log.lua
@@ -1,1 +1,12 @@
+local HttpService = game:GetService("HttpService")
 
+HttpService.HttpRequestCompleted:Connect(function(request, response, isSuccessful)
+    print("HTTP Request Completed:")
+    print("Request URL:", request.Url)
+    print("Request Method:", request.Method)
+    print("Request Headers:", HttpService:JSONEncode(request.Headers))
+    print("Request Body:", request.Body)
+    print("Response Status Code:", response.StatusCode)
+    print("Response Headers:", HttpService:JSONEncode(response.Headers))
+    print("Response Body:", response.Body)
+end)
